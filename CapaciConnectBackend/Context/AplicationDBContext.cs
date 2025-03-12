62acfb350cc8a18b1c357756cdbae57febc73741
0-EWT-0 <iamewt@gmail.com>
2025-03-11 20:13:48 -0400

    configuration

---
+++ CapaciConnectBackend/Context/AplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CapaciConnectBackend.Context
+{
+    public class AplicationDBContext : DbContext
+    {
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options) : base(options) 
+        {
+        
+        }
+
+    }
+}
---
+++ CapaciConnectBackend/Program.cs
@@ -0,0 +1,54 @@
+using CapaciConnectBackend.Context;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("https://localhost:5173") //puerto del front
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
+builder.Services.AddDbContext<AplicationDBContext>(options =>
+ {
+     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+ });
+
+//// Controllers + JSON Options
+//builder.Services.AddControllers().AddJsonOptions(options =>
+//{
+//    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+//    options.JsonSerializerOptions.WriteIndented = true;
+//});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("AllowFrontend");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
